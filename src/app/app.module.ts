4e016cabc256c706f435acceeae58fa6e5aa1f20
WuYoTing <hakosaki314@gmail.com>
2021-10-17 17:16:28 +0800

    feat: add home page call weahter api and add env data ignore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import {AppComponent} from './app.component';
 import {TopNavComponent} from './pages/layouts/top-nav/top-nav.component';
@@ -27,6 +28,8 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    // import HttpClientModule after BrowserModule.
+    HttpClientModule,
     RouterModule,
     MenubarModule,
     TabMenuModule,
--- src/app/pages/content/home/home.component.ts
+++ src/app/pages/content/home/home.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {environment} from "../../../../environments/environment";
 import {Title} from '@angular/platform-browser';
+import {HttpClient, HttpHeaders, HttpResponse} from "@angular/common/http";
 
 
 @Component({
@@ -9,11 +11,31 @@ import {Title} from '@angular/platform-browser';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private titleService: Title) {
+  weatherData!: any;
+
+  constructor(private titleService: Title, private http: HttpClient) {
   }
 
   ngOnInit(): void {
     this.titleService.setTitle('Home');
+    this.getWeatherData();
+  }
+
+  getWeatherData() {
+    let position = "Taipei";
+    let url = `${environment.weatherDomain}?q=${position}&appid=${environment.weatherAppId}&lang=${environment.language}&units=${environment.units}`;
+    this.http.get<any>(url, {observe: 'response'}).subscribe(res => {
+      console.log(res.body);
+      let status: number = res.status;
+      let response: HttpResponse<any> = res;
+      let statusText: string = res.statusText;
+      let headers: HttpHeaders = res.headers;
+      if (status == 200) {
+        this.weatherData = res.body;
+      } else {
+        alert("Error");
+      }
+    });
   }
 
 }
