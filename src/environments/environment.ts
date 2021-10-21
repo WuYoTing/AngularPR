c328bd92591f39b56a8c07c450ea7e7134ebf730
Tim <timyt.wu@wpgholdings.com>
2021-10-21 13:55:18 +0800

    chore: add env.ts and upadate push-deploy.yml

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  weatherDomain: 'https://api.openweathermap.org/data/2.5/weather',
+  weatherAppId: '',
+  language: 'zh_TW',
+  units: 'metric'
+};
